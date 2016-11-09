a135b1d005725ee6d7b06ea93d1df460484819fc
Jimmy Karl Roland Wärting <jimmy@warting.se>
2016-11-09 21:06:40 +0100

    enforce https

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import {Weather} from './weather';
 
-const API = 'http://api.openweathermap.org/data/2.5/weather?appid=224a29a9b7f8c996c7ad3d9db4678d33&q=';
+
+// openweathermap free api didn't support https...
+// my gh-page enforce https...
+const cors = 'https://crossorigin.me/'
+const API = cors + 'http://api.openweathermap.org/data/2.5/weather?appid=224a29a9b7f8c996c7ad3d9db4678d33&q=';
 
 var self
 
